31d40efd71c38c539a68d077cce3a9ecd469ba76
zverugin8 <zverugin8@gmail.com>
2022-06-26 21:46:08 +0300

    mask or cird func added

--- t1-5.ps1
+++ t1-5.ps1
@@ -1,4 +1,5 @@
-function test-mask {
+function test-mask_cidr
+{
     [cmdletbinding()]
     param (
     [parameter(Mandatory=$true,
@@ -6,18 +7,13 @@ function test-mask {
     [IPAddress]$ip9 # input mask value
     )
     #[IPAddress]$ip8="128.192.0.0"
-    $res="" 
-    
-    
-    if ( ($a -gt 0) -and ($a -le 32) 
-       -and    
-       )                            
-    
-    
-    
-    { $res=$true } # is digit 255? - then ok
-    elseif ( $($const_ar | where {$_ -eq $a}) -and ($next -eq 0) )  { $res=$true; $next=1 } # is digit in arr? - yes, but next digit must be zero
-    elseif ( $a -ne 0 )                          { $res=$False; $next=1} # is digit not zero? = incorrect mask 
-    Write-Verbose "a is $a; res is $res ; next is $next"
-    }
+    $res=""
+    $ress1=$ip9.GetAddressBytes()[0]
+    $ress2=( Compare-Object -ReferenceObject $ip9.GetAddressBytes()[1..3] -DifferenceObject @(0,0,0) )
+    Write-Host "ress1 is $ress1"
+    Write-Host "ress2.l is $($ress2.Length)"
+    if ( ($ress1 -gt 0) -and ($ress1 -le 32) -and ($ress2.Length -eq 0) ) {$res="cidr"} 
+    else {$res="mask"}
     return $res
+}
+    
