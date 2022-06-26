1e9be1eaca33e4bdb2d437b85a4835811f7ee44d
zverugin8 <zverugin8@gmail.com>
2022-06-26 22:03:50 +0300

    all functions done

--- t1-5.ps1
+++ t1-5.ps1
@@ -6,10 +6,13 @@ function test-mask_cidr
     Position=0)]
     [IPAddress]$ip9 # input mask value
     )
-    
+    $ip9.GetAddressBytes()[0]
+    $ip9.GetAddressBytes()[1]
+    $ip9.GetAddressBytes()[2]
+    $ip9.GetAddressBytes()[3]
     $res=""
-    $ress1=$ip9.GetAddressBytes()[0]
-    $ress2=( Compare-Object -ReferenceObject $ip9.GetAddressBytes()[1..3] -DifferenceObject @(0,0,0) )
+    $ress1=$ip9.GetAddressBytes()[3]
+    $ress2=( Compare-Object -ReferenceObject $ip9.GetAddressBytes()[0..2] -DifferenceObject @(0,0,0) )
     Write-verbose "ress1 is $ress1"
     Write-Verbose "ress2.l is $($ress2.Length)"
     if ( ($ress1 -gt 0) -and ($ress1 -le 32) -and ($ress2.Length -eq 0) ) {$res="cidr"} 
