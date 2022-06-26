85857d42ced6d59370bc1810b42635624cf4fec6
zverugin8 <zverugin8@gmail.com>
2022-06-26 15:25:16 +0300

    continuous mask chech add

---
+++ switch.ps1
@@ -0,0 +1,9 @@
+switch ( 9 )
+{
+    0 { 'Sunday' ;break   }
+    1 { 'Monday' ;break   }
+    2 { 'Tuesday';break   }
+    default { 'Wednesday' }
+   
+}
+
--- t1-4.ps1
+++ t1-4.ps1
@@ -1,16 +1,14 @@
-[IPAddress]$ip8="255.255.255.24"
-$const_ar=@(128,192,224,240,248,252,254,255)
-$nmask=0
-foreach ($a in $ip8.GetAddressBytes() ) 
-{
-    #if ( $const_ar -contains $a )
-    $const_ar | where {$_ -eq $a} #| select -First 1 | %{Write-Host "$a is in array"
+[IPAddress]$ip8="255.192.2.0"
+$const_ar=@(128,192,224,240,248,252,254) #mask possible values
+[boolean]$res=$False 
+$next=0
 
-    #Write-Host "$a is not in array"
+foreach ($a in $ip8.GetAddressBytes() )
+{
+ 
+   if ( $a -eq 255 )                            { $res=$true } # is digit 255? - then ok
+   elseif ( $($const_ar | where {$_ -eq $a}) -and ($next -eq 0) )  { $res=$true; $next=1 } # is digit in arr? - ok, but nex must be zero
+   elseif ( $a -ne 0 )                          { $res=$False; $next=1} # is digit not zero? = incorrect mask 
+   #else {$res=$false}
+   Write-Host "a is $a; res is $res ; next is $next"
 }
- #   Write-host $a
- #   for ($i = 0; $i -le 7 ; $i++) {
- #    $nmask+=( [math]::pow( 2,(7-$i) ) )
- #    write-host $nmask
- #    }
-#}
