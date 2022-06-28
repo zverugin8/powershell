012d99192b2efb548eab4ffa66bf6d653ce29526
Serg Soroka <zverugin8@gmail.com>
2022-06-28 17:28:36 +0300

    try solve EQ email problem - for

--- t2-4.ps1
+++ t2-4.ps1
@@ -1,8 +1,15 @@
 $arr1=$(Import-Csv -Path .\accounts_new.csv)
-
+$dupl_str=""
 for ($i = 0; $i -lt ($arr1.Count-1); $i++) {
     #$f_dupl=$arr1[$i].email
+    $flag=0
     for ($j = ($i+1); $j -lt $arr1.Count; $j++)  {
-    if ( $arr1[$i].email -eq $arr1[$j].email) {echo "$($arr1[$i].email) dulpicate, index $i $j"}
-    }
-}
+        if ( $arr1[$i].email -eq $arr1[$j].email) {
+            #echo "$($arr1[$i].email) dulpicate, index $i $j"
+            [string]$dupl_str+=" $j"
+            $flag=1
+        } #if
+    } #for $j
+    if ($flag -eq 1) {[string]$dupl_str+=" $i"}
+} #for $i
+echo $dupl_str
